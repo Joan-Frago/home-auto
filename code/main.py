c74b0be99f2cc8151c36c21f1f3deddcc71a1e43
Joan Frago <jfragojimenez@gmail.com>
2025-03-22 17:55:05 +0000

    Api to read a pin's state

--- code/main.py
+++ code/main.py
@@ -1,17 +1,22 @@
-#!/bin/python3
-
+#!/opt/home-auto/venv3.11/bin/python
 
 # General modules
 import os
 import sys
 import atexit
 
+# Dedicated modules
+import subprocess
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
 # Personal modules
-sys.path.append("/opt/dev/Python-Utils/utils/")
+sys.path.append("/opt/Python-Utils/utils/")
 from utils import Logger,DataBase,Timer,GetFuncName
 
 # Init logger
-_logger=Logger(log_path="/opt/dev/home-auto/log/home.log")
+_logger=Logger(log_path="/opt/home-auto/log/home.log")
 _logger.info("Started program")
 # Unhandled exceptions
 sys.excepthook = _logger.exception_handler
@@ -30,3 +35,30 @@ except Exception as e:
 	err = str(e)
 	_logger.error(f"Could not register execution time : {e}")
 
+
+# Init fastapi
+try:
+	app = FastAPI()
+	origins = ["http://192.168.1.100","http://100.74.114.37"]
+	app.add_middleware(CORSMiddleware,allow_origins=origins,allow_credentials=True,allow_methods=["*"],allow_headers=["*"])
+except Exception as e:
+	err = "Could not start api" + ":" + str(e) + str(sys.exc_info())
+	_logger.error(err)
+
+class DigitalPin:
+	def __init__(self,aPin:str):
+		self.iPin=aPin # 2.(pin)
+	
+	def read(self):
+		status = subprocess.run(["cat", f"/run/unipi-plc/by-sys/DI{self.iPin}/value"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+		if status.returncode != 0: return f"Error {status.returncode} : {status.stderr.decode().strip()}"
+		else: return str(status.stdout.decode().strip())
+
+@app.get("/api/ReadPin/{iPin}")
+async def ReadPin(iPin:str):
+	pin = DigitalPin(aPin=iPin)
+	return {"status":str(pin.read())}
+
+if __name__ == "__main__":
+	uvicorn.run(app, host="0.0.0.0", port=8000)
